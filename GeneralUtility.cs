1b157705e12c955bd9cf7f7a5277e8757685561c
HuseyinAkay <huseyin.i.akay@gmail.com>
2022-09-13 07:31:17 +0300

    Added a vector and a general utility script

---
+++ GeneralUtility.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityExtensions.General
+{
+   public static class GeneralUtility
+   {
+      /// <summary>checks if the object belongs to a prefab on project files or a GameObject inside a scene</summary>
+
+      /// <returns>true if the object belongs to a prefab false otherwise</returns>
+      public static bool isPrefab(this GameObject obj) => obj.scene.name == null;
+
+
+
+      /// <summary>checks if the object belongs to a prefab on project files or a GameObject inside a scene</summary>
+
+      /// <returns>true if the object belongs to a prefab false otherwise</returns>
+      public static bool isPrefab(this Component obj) => obj.gameObject.scene.name == null;
+
+
+
+      /// <summary>colors the string using html</summary>
+
+      /// <param name="color">color of the returned text</param>
+
+      /// <returns>the original string with added html color tags before and after</returns>
+      public static string GetColoredStringHTML(this string text, Color color) => "<color=#" + UnityEngine.ColorUtility.ToHtmlStringRGBA(color) + ">" + text + "</color>";
+   }
+}
+
---
+++ VectorUtilities.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityExtensions.Vectors
+{
+   public static class VectorUtilities
+   {
+      /// <summary>makes a <c>Vector3</c> world position two dimensional</summary>
+
+      /// <returns>a <c>Vector2</c> from original values x and z elements</returns>
+      public static Vector2 ToTopdown(this Vector3 vector) => new Vector2(vector.x, vector.z);
+
+
+      /// <summary>makes a <c>Vector2</c> topdown world position three dimensional</summary>
+
+      /// <param name="y">optional y element of returned value</param>
+
+      /// <returns>a <c>Vector3</c> from original value's x, optional <paramref name="y"/> parameter and original value's z element</returns>
+      public static Vector3 FromTopdown(this Vector2 vector, float y = 0) => new Vector3(vector.x, y, vector.y);
+   }
+}
